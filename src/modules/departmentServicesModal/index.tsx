3b8823b4ef8752db363d31b73f64b22662a9845c
TARO ABDOUL-AZIZ <58216243+Aztaro97@users.noreply.github.com>
2024-05-15 14:41:18 +0000

    feat: add service pagination

--- src/modules/departmentServicesModal/index.tsx
+++ src/modules/departmentServicesModal/index.tsx
@@ -12,13 +12,14 @@ import {
 } from "@/components/ui/dialog";
 
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { HoverEffect } from "@/components/ui/card-hover-effect";
@@ -28,6 +29,7 @@ import HassantukBuildingIcon from "@/icons/hassantukBuildingIcon";
 import LicenceSafetyIcon from "@/icons/licenceSafetyIcon";
 import LicenseRenewIcon from "@/icons/licenseRenewIcon";
 import InssuanceIcon from "@/icons/inssuanceIcon";
+import { Search as SearchIcon } from "lucide-react";
 
 interface TService {
   title: string;
@@ -87,10 +89,44 @@ export default function DepartmentServicesModal() {
           <DialogTitle className="text-white text-5xl mb-10 uppercase">
             <span className="text-primary">Civil defense</span> services
           </DialogTitle>
+          <div className="flex w-full max-w-sm items-center space-x-2">
+            <Input
+              className="rounded-lg"
+              placeholder="Search for services"
+              type="search"
+            />
+            <Button className="rounded-lg" type="submit">
+              <SearchIcon />
+            </Button>
+          </div>
         </DialogHeader>
         <div className=" h-full  w-full">
           <HoverEffect items={services} />
         </div>
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#" isActive>
+                1
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">2</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">3</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
       </DialogContent>
     </Dialog>
   );
