1ccf387da1677e7c71ef23b94d73fe4cd27604b3
TARO ABDOUL-AZIZ <58216243+Aztaro97@users.noreply.github.com>
2024-05-15 18:58:56 +0000

    style: integrate step form two

--- src/app/(eservice)/traffic-services/form/page.tsx
+++ src/app/(eservice)/traffic-services/form/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import PoliticingFormStepOne from "@/modules/politicing-services/politicing-services-form/politicing-form-step-one";
-import PoliticingFormStepThree from "@/modules/politicing-services/politicing-services-form/politicing-form-step-three";
-import PoliticingFormStepTwo from "@/modules/politicing-services/politicing-services-form/politicing-form-step-two";
+import PoliticingService from "@/modules/politicing-services";
 import { usePoliticingFormStepStore } from "@/stores/services/usePoliticingFormStepStore";
 import React from "react";
 
@@ -18,19 +16,6 @@ const Page: React.FC = () => {
     }
   };
 
-  const renderStep = () => {
-    switch (currentStep) {
-      case 1:
-        return <PoliticingFormStepOne />;
-      case 2:
-        return <PoliticingFormStepTwo />;
-      case 3:
-        return <PoliticingFormStepThree />;
-      default:
-        return null;
-    }
-  };
-
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[2fr_5fr] justify-center items-center min-h-screen">
       <div className="p-4">
@@ -39,7 +24,7 @@ const Page: React.FC = () => {
           the next step button
         </p>
       </div>
-      <div className="w-full">{renderStep()}</div>
+      <PoliticingService />
     </div>
   );
 };
---
+++ src/modules/politicing-services/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import PoliticingFormStepOne from "@/modules/politicing-services/politicing-services-form/politicing-form-step-one";
+import PoliticingFormStepThree from "@/modules/politicing-services/politicing-services-form/politicing-form-step-three";
+import PoliticingFormStepTwo from "@/modules/politicing-services/politicing-services-form/politicing-form-step-two";
+import { usePoliticingFormStepStore } from "@/stores/services/usePoliticingFormStepStore";
+
+export default function PoliticingService() {
+  const { setCurrentStep, currentStep } = usePoliticingFormStepStore();
+
+  const renderStep = () => {
+    switch (currentStep) {
+      case 1:
+        return <PoliticingFormStepOne />;
+      case 2:
+        return <PoliticingFormStepTwo />;
+      case 3:
+        return <PoliticingFormStepThree />;
+      default:
+        return null;
+    }
+  };
+  return (
+    <div className="">
+      <h1>Service Description</h1>
+      <p className="text-sm">
+        By this service, you may apply to create a new traffic file "First
+        time", or apply to create another traffic file "To obtain a new traffic
+        category license", Please note that once the application is approved,
+        you must return to the service page to complete your application.
+      </p>
+      <div className="w-full mt-10">{renderStep()}</div>
+    </div>
+  );
+}
