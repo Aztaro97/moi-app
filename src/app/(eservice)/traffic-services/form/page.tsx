006fc96b827eab234bdd9727a8cb21c13a530379
aztaro97 <aztaro97gmail.com>
2024-05-16 00:46:25 +0400

    chore: update the traffic tuto data

--- src/app/(eservice)/traffic-services/form/page.tsx
+++ src/app/(eservice)/traffic-services/form/page.tsx
@@ -8,19 +8,22 @@ import TutorialVideo from "@/components/tutorial-video";
 const trafficTutorialSteps = [
   {
     title: "How to fill application info",
-    description: " ",
+    description:
+      "In this step, you'll learn how to fill out the application form. We'll go over each field and explain what information you need to provide.",
     thumbnail: "/images/fire.jpeg",
     video: "/videos/demo.mp4",
   },
   {
     title: "Checking restrictions",
-    description: "",
+    description:
+      "This step will guide you through the process of checking for any restrictions that might apply to you. We'll show you where to find this information and how to interpret it.",
     thumbnail: "/images/driving.jpeg",
     video: "/videos/demo.mp4",
   },
   {
     title: "How to make payment",
-    description: "",
+    description:
+      "In the final step, we'll walk you through the payment process. We'll explain how to choose a payment method, enter your payment details, and confirm your payment.",
     thumbnail: "/images/gun.jpeg",
     video: "/videos/demo.mp4",
   },
