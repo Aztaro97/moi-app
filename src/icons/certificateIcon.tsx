73f0a99b495c83a84b7afcb2c99040cc288934e0
TARO ABDOUL-AZIZ <58216243+Aztaro97@users.noreply.github.com>
2024-05-15 14:26:41 +0000

    feat: create civil defense service

---
+++ src/icons/certificateIcon.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+function CertificateIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="50"
+      height="50"
+      viewBox="0 0 53.559 63.206"
+    >
+      <g
+        fill="none"
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        transform="translate(-56.169 -1466.229)"
+      >
+        <g strokeWidth="2" data-name="Group 873">
+          <path
+            d="M79.649 1486.025l-1.233-1.233h-3.083"
+            data-name="Path 2673"
+          ></path>
+          <path
+            d="M73.483 1489.724v1.85a.616.616 0 00.617.616h4.316a.616.616 0 00.616-.616"
+            data-name="Path 2674"
+          ></path>
+          <path
+            d="M97.53 1478.625h-3.083l-1.233 1.85-1.85.617H77.8l-1.85-.617-1.233-1.85h-3.084a.616.616 0 00-.616.617v1.233a.616.616 0 00.616.617l2.467.616-.612.918a3.7 3.7 0 00-.622 2.052v3.2a1.85 1.85 0 001.85 1.85h19.731a1.85 1.85 0 001.85-1.85v-3.2a3.7 3.7 0 00-.621-2.052l-.612-.918 2.466-.616a.617.617 0 00.617-.617v-1.233a.617.617 0 00-.617-.617z"
+            data-name="Path 2675"
+          ></path>
+          <path
+            d="M93.831 1479.241s-1.567-2.746-1.85-3.082a1.8 1.8 0 00-1.233-.617H78.416a1.794 1.794 0 00-1.233.617c-.284.336-1.85 3.082-1.85 3.082"
+            data-name="Path 2676"
+          ></path>
+          <path
+            d="M89.514 1486.025l1.233-1.233h3.083"
+            data-name="Path 2677"
+          ></path>
+          <path
+            d="M95.681 1489.724v1.85a.617.617 0 01-.617.616h-4.316a.616.616 0 01-.617-.616"
+            data-name="Path 2678"
+          ></path>
+        </g>
+        <path
+          strokeWidth="2"
+          d="M19.835 0L0 0"
+          data-name="Line 803"
+          transform="translate(79.973 1504.106)"
+        ></path>
+        <path
+          strokeWidth="2"
+          d="M10.63 0L0 0"
+          data-name="Line 804"
+          transform="translate(89.177 1509.002)"
+        ></path>
+        <path
+          strokeWidth="2"
+          d="M30.881 0L0 0"
+          data-name="Line 805"
+          transform="translate(69.141 1499.315)"
+        ></path>
+        <g strokeWidth="2" data-name="Group 874">
+          <path d="M108.728 1525.19H60.435v-49.04" data-name="Path 2679"></path>
+          <path d="M69.356 1467.229h39.372v57.961" data-name="Path 2680"></path>
+          <path d="M60.435 1476.15h8.921v-8.921z" data-name="Path 2681"></path>
+        </g>
+        <path
+          strokeWidth="1"
+          d="M105.934 1527.258H58.346v-49.1"
+          data-name="Path 2682"
+        ></path>
+        <path
+          strokeWidth="1"
+          d="M107.328 1528.935H56.669v-49.1"
+          data-name="Path 2683"
+        ></path>
+      </g>
+    </svg>
+  );
+}
+
+export default CertificateIcon;
--- src/modules/departmentServicesModal/index.tsx
+++ src/modules/departmentServicesModal/index.tsx
@@ -11,6 +11,73 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { HoverEffect } from "@/components/ui/card-hover-effect";
+import HassantukIcon from "@/icons/hassantukIcon";
+import CertificateIcon from "@/icons/certificateIcon";
+import HassantukBuildingIcon from "@/icons/hassantukBuildingIcon";
+import LicenceSafetyIcon from "@/icons/licenceSafetyIcon";
+import LicenseRenewIcon from "@/icons/licenseRenewIcon";
+import InssuanceIcon from "@/icons/inssuanceIcon";
+
+interface TService {
+  title: string;
+  icon: React.ReactNode;
+  description: string;
+  link: string;
+}
+
+export const services: TService[] = [
+  {
+    title: "",
+    icon: <HassantukIcon />,
+    description: "Hassantuk For Homes",
+    link: "/game/Hassantuk",
+  },
+  {
+    title: "",
+    icon: <CertificateIcon />,
+    description:
+      "Issue a certificate of conformity with preventive safety requirements for low risk buildings and facilities A",
+    link: "/game/Hassantuk",
+  },
+  {
+    title: "",
+    icon: <HassantukBuildingIcon />,
+    description: "Hassantuk for Buildings",
+    link: "/game/Hassantuk",
+  },
+  {
+    title: "",
+    icon: <LicenceSafetyIcon />,
+    description:
+      "Licensing a Distributor in the field of preventive fire safety",
+    link: "/game/Hassantuk",
+  },
+  {
+    title: "",
+    icon: <LicenseRenewIcon />,
+    description:
+      "Licensing a Distributor in the field of preventive fire safety",
+    link: "/game/Hassantuk",
+  },
+  {
+    title: "",
+    icon: <InssuanceIcon />,
+    description: `Issuance of "To Whom It May Concern" Certificate - Civil Defense`,
+    link: "/game/Hassantuk",
+  },
+];
+
 export default function DepartmentServicesModal() {
   const { isOpen, setIsOpen } = useDepartmentServicesModal();
   return (
@@ -18,11 +85,11 @@ export default function DepartmentServicesModal() {
       <DialogContent className="max-w-[1000px] w-full bg-background text-foreground p-8">
         <DialogHeader>
           <DialogTitle className="text-white text-5xl mb-10 uppercase">
-            Civil defense services
+            <span className="text-primary">Civil defense</span> services
           </DialogTitle>
         </DialogHeader>
-        <div className=" h-full grid grid-cols-[1fr_5fr] gap-10">
-          <h1>Service Modal</h1>
+        <div className=" h-full  w-full">
+          <HoverEffect items={services} />
         </div>
       </DialogContent>
     </Dialog>
